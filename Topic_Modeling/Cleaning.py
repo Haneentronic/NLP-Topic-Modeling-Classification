aac4a54750c82b498f07f174f6d605a2800cdb1b
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2023-05-16 16:30:46 +0300

    final commit

--- Topic_Modeling/Cleaning.py
+++
@@ -1,6 +0,0 @@
-class Cleaning:
-    def __init__(self, data_frame):
-        self.data_frame = data_frame
-
-    def drop_column(self, column):
-        self.data_frame.drop(columns=[column], inplace=True)
